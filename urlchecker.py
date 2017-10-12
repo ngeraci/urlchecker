53876f64d5012418f70cb368e37b0ef480358c69
Noah Geraci <noah.geraci@ucr.edu>
2017-10-11 17:00:22 -0700

    add print stmt

--- urlchecker.py
+++ urlchecker.py
@@ -9,20 +9,21 @@ def main():
 	pd.options.mode.chained_assignment = None  
 
 	#set file path variables
-	infile = #'path to CSV input file'
-	outfile = #'path to CSV output file'
+	infile = 'C:/Users/ngeraci/Documents/core/ContentDMCleanup/wrca_millennium_urls.csv'
+	outfile = 'C:/Users/ngeraci/Documents/core/ContentDMCleanup/wrca_millennium_urls_status2.csv'
 
 	# Create a new dataframe from a csv file
 	df = pd.read_csv(infile)
 
 	#URLs from dataframe to list
-	urls = df['URL'].tolist()
+	urls = df['Test URL'].tolist()
 
 	#create empty list
 	urlStatus = []
 
 	#loop to get url status
 	for i in urls:
+		print ("checking " + str(i))
 		code = urllib.urlopen(i).getcode()
 		urlStatus.append(code)
 
@@ -42,7 +43,7 @@ def main():
 	df.to_csv(outfile, index=False)
 
 	# done
-	print("done")
+	print('done')
 
 if __name__ == '__main__':
     main()
